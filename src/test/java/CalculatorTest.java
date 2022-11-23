5b42c42eee93be4a86345a1849463ca3ae63b3c8
Demasto <mr.d1men@yandex.ru>
2022-11-23 11:29:45 +0300

    init

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+    @Test
+    void additional(){
+        assertEquals(2,calculator.add(1,1));
+    }
+    @Test
+    void difference(){
+        assertEquals(3,calculator.dif(7,4));
+    }
+    @Test
+    void multiplication(){
+        assertEquals(12,calculator.mul(4,3));
+    }
+    @Test
+    void division(){
+        assertEquals(3,calculator.div(27,7));
+    }
+}
